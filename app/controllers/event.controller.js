1ef0f31d52355b7de2b81bd35953c33ae80308b2
rangganast <rangganast@gmail.com>
2021-03-26 17:20:11 +0700

    modify findall and findone for frontend needs

--- app/controllers/event.controller.js
+++ app/controllers/event.controller.js
@@ -2,12 +2,34 @@ const { Event, Organizer } = require('../db/models');
 
 exports.findAll = async (req, res) => {
     Event.findAll({
+        attributes: ['id', 'name', 'description', 'image', 'eventStart'],
         include: {
             raw: true,
             model: Organizer,
             required:true,
             as: 'organizer',
-            attributes: ['name', 'image']
+            attributes: ['id', 'name', 'image']
+        }
+    })
+    .then(data => {
+        res.status(200).json(data);
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err.message
+        });
+    });
+};
+
+exports.findOne = async (req, res) => {
+    Event.findByPk(req.params.id, {
+        attributes: ['id', 'name', 'description', 'image', 'quota', 'registrationStart', 'registrationEnd', 'eventStart', 'eventEnd'],
+        include: {
+            raw: true,
+            model: Organizer,
+            required: true,
+            as: 'organizer',
+            attributes: ['id', 'name', 'image']
         }
     })
     .then(data => {
--- app/routers/event.router.js
+++ app/routers/event.router.js
@@ -2,6 +2,8 @@ let router = require('express').Router();
 const controller = require('../controllers/event.controller');
 
 // routers
+router.get('/:id', controller.findOne);
+
 router.get('/', controller.findAll);
 
 module.exports = router;
